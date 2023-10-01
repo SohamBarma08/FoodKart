44e14a256b7bf1bd35386e0ba6126daec693d2c9
Soham Barma <sohambarma483@gmail.com>
2023-10-01 22:06:52 +0530

    displaying cards

--- backend/db.js
+++ backend/db.js
@@ -6,10 +6,14 @@ const mongoDB = async () => {
     if (err) console.log("---", err);
     else {
       console.log("connected");
-      const fetched_data = await mongoose.connection.db.collection("food_items")
-      fetched_data.find({}).toArray(function(err,data){
-        if (err) console.log("---", err);
-        else console.log();
+      const fetched_data = await mongoose.connection.db.collection("food_items");
+      fetched_data.find({}).toArray(async function(err,data){
+        const foodCategory = await mongoose.connection.db.collection("food_category");
+
+        // if (err) console.log("---", err);
+        // else {
+        //   global.food_items = data;
+        // }
       })
     }
   });
