20bc49c119b70a987716d65ce16f46d7947e8906
Soham Barma <sohambarma483@gmail.com>
2023-10-02 23:29:29 +0530

    update

--- backend/db.js
+++ backend/db.js
@@ -9,7 +9,13 @@ const mongoDB = async () => {
       const fetched_data = await mongoose.connection.db.collection("food_items");
       fetched_data.find({}).toArray(async function(err,data){
         const foodCategory = await mongoose.connection.db.collection("food_category");
-
+        foodCategory.find({}).toArray(function (err,catData){
+          if (err) console.log("---", err);
+          else {
+               global.food_items = data;
+               global.foodCategory = catData;
+          }
+        })
         // if (err) console.log("---", err);
         // else {
         //   global.food_items = data;
